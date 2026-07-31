98df083d449db267a5b04ce4a3ec6e78dfcbec1e
alexwbt <alexwbtg@gmail.com>
2026-07-31 12:55:04 +0800

    update

--- lua/config/neotree.lua
+++ lua/config/neotree.lua
@@ -1,2 +1,14 @@
 
 vim.keymap.set("n", "<C-b>", "<cmd>Neotree toggle<cr>")
+
+local neotree = require("neo-tree")
+
+neotree.setup({
+  window = {
+    mappings = {
+      ["Z"] = "expand_all_nodes",
+      ["W"] = "close_all_nodes",
+    }
+  }
+})
+
