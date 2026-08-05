a10a39f855abf1a9e41cca0935c936c19e6fb0f3
alexwbt <alexwbtg@gmail.com>
2026-08-05 09:22:01 +0800

    update

--- lua/config/neotree.lua
+++ lua/config/neotree.lua
@@ -4,12 +4,6 @@ vim.keymap.set("n", "<leader>e", "<Cmd>Neotree<CR>")
 local neotree = require("neo-tree")
 
 neotree.setup({
-  window = {
-    mappings = {
-      ["Z"] = "expand_all_nodes",
-      ["W"] = "close_all_nodes",
-    }
-  },
   filesystem = {
     filtered_items = {
       visible = true,
