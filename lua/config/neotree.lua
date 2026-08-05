aeda0b61212fb59b792f84afa8923a22cdfe7339
alexwbt <alexwbtg@gmail.com>
2026-08-05 13:22:06 +0800

    update

--- lua/config/neotree.lua
+++ lua/config/neotree.lua
@@ -5,6 +5,11 @@ local neotree = require("neo-tree")
 
 neotree.setup({
   filesystem = {
+    bind_to_cwd = false,
+    follow_current_file = {
+      enabled = true,
+      leave_dirs_open = false,
+    },
     filtered_items = {
       visible = true,
     }
