dd14555f8cb98facc67b797ad8c01b3e3519ebf3
alexwbt <alexwbtg@gmail.com>
2026-08-18 11:28:14 +0800

    update

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -6,9 +6,7 @@ require("config.lsp.typescript")
 vim.keymap.set("n", "<F2>", vim.lsp.buf.rename)
 local lsp_code_action = function()
   vim.lsp.buf.code_action({
-    filter = function(action)
-      return not (action.disabled and action.disabled.reason)
-    end,
+    filter = function(action) return action.disabled == nil end
   })
 end
 vim.keymap.set("n", "<leader><space>", lsp_code_action, { desc = "LSP code action" })
