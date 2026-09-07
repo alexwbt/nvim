142b7b8a1a6ee5d0eba90b945d32db26d180ca81
alexwbt <alexwbtg@gmail.com>
2026-09-07 14:04:54 +0800

    update codebook config

--- lua/config/lsp/codebook.lua
+++ lua/config/lsp/codebook.lua
@@ -2,6 +2,7 @@ local jit = require("jit")
 
 vim.lsp.config("codebook", {
   cmd = { jit.os == "Windows" and "codebook-lsp.cmd" or "codebook-lsp", "serve" },
-  root_markers = { '.git', 'codebook.toml', '.codebook.toml' },
+  root_markers = { ".git", "codebook.toml", ".codebook.toml" },
+  exit_timeout = 2000,
 })
 vim.lsp.enable("codebook")
