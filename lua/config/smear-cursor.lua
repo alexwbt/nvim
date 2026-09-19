20ad2199f6b68a2529473a55eaeaacaea295019e
alexwbt <alexwbtg@gmail.com>
2026-09-19 10:50:34 +0800

    add smear cursor

---
+++ lua/config/smear-cursor.lua
@@ -0,0 +1,10 @@
+require("smear_cursor").setup({
+  cursor_color = "#777777",
+  transparent_bg_fallback_color = "#1e1e1e",
+  stiffness = 0.5,
+  trailing_stiffness = 0.49,
+  damping = 1.0,
+  damping_insert_mode = 1.0,
+  distance_stop_animating = 0.5,
+  time_interval = 5,
+})
