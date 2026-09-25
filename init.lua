35ad17668eaae71781266dcc1dd1afdf1d3e8c59
alexwbt <alexwbtg@gmail.com>
2026-09-26 00:59:28 +0800

    move windows config

--- init.lua
+++ init.lua
@@ -20,27 +20,6 @@ vim.opt.foldlevel = 99
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.cmd([[set fillchars+=vert:\ ]])
 
-if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
-  vim.g.is_windows = true
-  vim.opt.shell = "bash"
-  vim.opt.shellcmdflag = "-c"
-  vim.opt.shellquote = ""
-  vim.opt.shellxquote = ""
-  vim.opt.shellslash = true
-
-  -- Upstream gx/vim.ui.open bug: win32 hardcodes cmd.exe, but with 'shellslash'
-  -- exepath() yields a forward-slash path uv.spawn can't launch.
-  -- https://github.com/neovim/neovim/issues/39524
-  vim.ui.open = function(uri)
-    return vim.system(
-      { "rundll32", "url.dll,FileProtocolHandler", uri },
-      { detach = true }
-    )
-  end
-  vim.g.netrw_browsex_viewer = "rundll32 url.dll,FileProtocolHandler"
-end
-
-
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<C-k>", "10k")
@@ -103,6 +82,7 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 
+require("config.windows")
 require("config.jumplist")
 require("config.project-local")
 require("config.lsp")
